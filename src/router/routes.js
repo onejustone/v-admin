9c4bca2b92aec3ed29dceb059819fef17a01a042
chenrong <chenrong@lingxi360.com>
2018-01-09 10:11:29 +0800

    fix: format routes code

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,52 +8,20 @@ const constRoutes = [
 
   { name: '404', path: '/404', component: _import('error/error404'), meta: { title: '404' }, hidden: true },
 
-  {
-    path: '',
-    redirect: '/dashboard'
-  }
+  { path: '', redirect: '/dashboard' }
 ]
 
 const asyncRoutes = [
   {
-    name: 'dashboard',
-    path: '/dashboard',
-    component: Layout,
-    redirect: '/dashboard/index',
-    children: [
-      {
-        path: '/dashboard/index',
-        component: _import('dashboard/index'),
-        meta: {
-          title: '首页'
-        }
-      }
-    ]
+    path: '/dashboard', component: Layout, redirect: '/dashboard/index',
+    children: [{ name: 'home', path: '/dashboard/index', component: _import('dashboard/index'), meta: { title: '首页' }}]
   },
+
   {
-    path: '/components',
-    name: 'components',
-    component: Layout,
-    redirect: '/components/båck_to_up',
-    meta: { icon: 'document', title: '组件', roles: ['admin'] },
+    name: 'components', path: '/components', component: Layout, redirect: '/components/båck_to_up', meta: { icon: 'document', title: '组件', roles: ['admin'] },
     children: [
-      {
-        path: '/components/båck_to_up',
-        name: 'backto',
-        component: _import('components/BackToUp'),
-        meta: {
-          title: '回到顶部',
-          roles: ['admin']
-        }
-      },
-      {
-        path: '/components/test_menu',
-        name: 'test-menu',
-        component: _import('components/TestMenu'),
-        meta: {
-          title: 'test-menu'
-        }
-      }
+      { name: 'backto', path: '/components/båck_to_up', component: _import('components/BackToUp'), meta: { title: '回到顶部', roles: ['admin'] }},
+      { name: 'test-menu', path: '/components/test_menu', component: _import('components/TestMenu'), meta: { title: 'test-menu' }}
     ]
   }
 ]
