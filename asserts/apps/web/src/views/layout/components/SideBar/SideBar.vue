d7a8162f8790a735d7e50f818dab5893a05f59ac
chenrong <chenrong@lingxi360.com>
2019-02-20 19:58:37 +0800

    fix: use el-scrollbar to warp sidebar

--- asserts/apps/web/src/views/layout/components/SideBar/SideBar.vue
+++ asserts/apps/web/src/views/layout/components/SideBar/SideBar.vue
@@ -1,20 +1,27 @@
 <template lang="pug">
   section(class="side-bar" style="height:100%")
-    el-menu(
-      style="height:100%"
-      mode="vertical"
-      @open="handleOpen"
-      @close="handleClose"
-      :default-active="$route.path"
-      :default-openeds="openedsRoute"
-      :collapse="isCollapse"
-      background-color="#304156"
-      text-color="white"
-      active-text-color="#409EFF"
+    el-scrollbar.side-bar-scrollbar(
+      wrap-class="app-side-bar__wrap-class"
+      wrap-style="height: 100%"
+      view-class="app-side-bar__view-class"
+      view-style="height: 100%"
+      :native="false"
     )
-      //- a(href="screen.html")
-      //-   el-menu-item(index="screen") 屏保应用
-      side-bar-item(:routes="routes")
+      el-menu(
+        style="height:100%"
+        mode="vertical"
+        @open="handleOpen"
+        @close="handleClose"
+        :default-active="$route.path"
+        :default-openeds="openedsRoute"
+        :collapse="isCollapse"
+        background-color="#304156"
+        text-color="white"
+        active-text-color="#409EFF"
+      )
+        //- a(href="screen.html")
+        //-   el-menu-item(index="screen") 屏保应用
+        side-bar-item(:routes="routes")
 </template>
 
 <script>
@@ -53,9 +60,16 @@
   }
 </script>
 
-<style lang="stylus">
-  .side-bar
-    overflow auto
-    .el-menu:not(.el-menu--collapse)
-      min-width 200px
+<style lang="scss" scoped>
+  .side-bar-scrollbar {
+    height: 100%;
+  }
+
+  .side-bar {
+    overflow: auto;
+
+    .el-menu:not(.el-menu--collapse) {
+      min-width: 200px;
+    }
+  }
 </style>
